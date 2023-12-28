eb135b5f9ade840a8128c615887f751c4cc0e093
Tyler Boyd <ty@tyboyd.dev>
2023-12-28 15:17:26 -0600

    initial commit

---
+++ components/NewEntry.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// script
+</script>
+
+<template>
+  <p>hey</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NewEntry from "~/components/NewEntry.vue";
+</script>
+
+<template>
+  <div>
+    <NewEntry/>
+  </div>
+</template>
